2ca89b0058663bbf278f469af828ceaf5d2e0e45
Aleksandr Korol <ermvs88ryv57@mail.ru>
2020-12-05 20:25:07 +0300

    Add readme

--- HW21/disk_usage.sh
+++ HW21/disk_usage.sh
@@ -11,11 +11,11 @@ if [[ $disk_mount != "boot" ]]
  then
   if (($disk_usage>70 && $disk_usage<80))
    then
-    echo "WARNING - $disk_name DISK USAGE MORE THEN 70%" | mail -s "DISK USAGE" alex_king87@mail.ru
+    echo "WARNING - $disk_name DISK USAGE MORE THEN 70%" | mail -s "DISK USAGE" ermvs88ryv57@mail.ru
    elif (($disk_usage>80 && $disk_usage<90 ))
     then
      top10=`find . / -type f -exec du {} 2>/dev/null \;|sort -nr | head`
-     echo "WARNING - $disk_name DISK USAGE MORE THEN 80%. TOP 10 FILES WITH BIG SIZE - $top10"
+     echo "WARNING - $disk_name DISK USAGE MORE THEN 80%. TOP 10 FILES WITH BIG SIZE - $top10" |mail -s "DISK USAGE" ermvs88ryv57@mail.ru
    elif (( $disk_usage>90 ))
     then
       for i in `find /var/log -type f -exec du {} 2>/dev/null \;|sort -nr | head -n 2 | cut -f2`
